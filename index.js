4627e8945513e532bf9b6cb91e4c08b6cec2b9b2
Felipe Oshiro <felipeoshiro2004@gmail.com>
2025-09-28 22:22:57 -0300

    feat(p1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const axios = require('axios')
 
 const { PROTOCOL, BASE_URL, APPID, Q } = process.env
 
-const geocodingURL = `${PROTOCOL}://${BASE_URL}/geo/1.0/direct?q=${encodeURIComponent(Q)}&limit=1&appid=${APPID}`
-
-axios.get(geocodingURL)
-  .then(res => {
-    const place = res.data[0]
+async function getWeather() {
+  try {
+    const geocodingURL = `${PROTOCOL}://${BASE_URL}/geo/1.0/direct?q=${encodeURIComponent(Q)}&limit=1&appid=${APPID}`
+    const geoRes = await axios.get(geocodingURL)
+    
+    const place = geoRes.data[0]
     console.log(`Cidade: ${place.name}, Latitude: ${place.lat}, Longitude: ${place.lon}`)
-  })
-  .catch(err => console.error('Erro:', err.message))
+
+    const weatherURL = `${PROTOCOL}://${BASE_URL}/data/2.5/weather?lat=${place.lat}&lon=${place.lon}&appid=${APPID}&units=metric&lang=pt_br`
+    const weatherRes = await axios.get(weatherURL)
+
+    const { feels_like } = weatherRes.data.main
+    const description = weatherRes.data.weather[0].description
+
+    console.log(`Sensação térmica: ${feels_like}°C`)
+    console.log(`Descrição: ${description}`)
+
+  } catch (err) {
+    console.error('Erro na consulta:', err.message)
+  }
+}
+
+getWeather()
